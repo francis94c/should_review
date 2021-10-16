76a33813e2609ea4e40ed5b22ca62dcd5174daac
Francis Ilechukwu <francis94c@gmail.com>
2021-10-16 22:05:05 +0100

    Initial Commit.

---
+++ lib/config/pref_keys.dart
@@ -0,0 +1,4 @@
+const String _keyPrefix = "should_review_";
+const String prefTimesOpened = "${_keyPrefix}times_opened";
+const String prefInstalledDate = "${_keyPrefix}installed_date";
+const String prefLastReviewPromptDate = "${_keyPrefix}last_review_prompt_date";
---
+++ lib/should_review.dart
@@ -0,0 +1,8 @@
+library should_review;
+
+/// Should Review Class.
+class ShouldReview {
+  static Future<void> shouldReview() async {}
+}
+
+enum Criteria { timesOpened, days }
